3367eb95d54e5a91a6af30e11957def3703a3048
ehdrbdndns <ehdrbdndns@naver.com>
2024-11-21 17:07:25 +0900

    feat: add splash image file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,47 @@
+import {
+  NotoSansKR_400Regular,
+  NotoSansKR_500Medium,
+  NotoSansKR_700Bold,
+  useFonts,
+} from '@expo-google-fonts/noto-sans-kr';
 import { Stack } from "expo-router";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
-  return <Stack />;
+
+  let [fontsLoaded] = useFonts({
+    NotoSansKR_400Regular,
+    NotoSansKR_500Medium,
+    NotoSansKR_700Bold,
+  });
+
+  const [isAppReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      setTimeout(() => {
+        SplashScreen.hideAsync();
+        setAppReady(true);
+      }, 2000);
+    }
+  }, [fontsLoaded])
+
+  if (!isAppReady) {
+    return null;
+  }
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
 }
