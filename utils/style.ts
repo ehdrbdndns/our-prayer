2fe783c5b729bf7f6c306a9f0b0c70d6d30fb3f3
ehdrbdndns <ehdrbdndns@naver.com>
2024-11-25 19:00:29 +0900

    feat: add responsive scaling utilities for components

---
+++ utils/style.ts
@@ -0,0 +1,22 @@
+import { Dimensions } from "react-native";
+
+const BASE_WIDTH = 375;
+const BASE_HEIGHT = 812;
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+
+const widthRatio = deviceWidth / BASE_WIDTH;
+const heightRatio = deviceHeight / BASE_HEIGHT;
+
+// Scale the size of the component based on the screen size
+export const scale = (size: number) => widthRatio * size;
+
+// Scale the size of the component based on the screen height
+export const verticalScale = (size: number) => heightRatio * size;
+
+// Scale the size of the component based on the screen width with a factor( default is 0.5 )
+export const moderateScale = (size: number, factor = 0.5) => size + (scale(size) - size) * factor;
+
+// Normalize the font size based on the screen width
+export const normalizeFontSize = (size: number) => Math.round(size * widthRatio);
