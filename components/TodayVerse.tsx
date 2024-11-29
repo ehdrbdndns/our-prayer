7f720ec92d133d7fc19c0a23f68d07e1464157c0
ehdrbdndns <ehdrbdndns@naver.com>
2024-11-30 00:55:39 +0900

    feat: add TodayVerse component for displaying daily scripture with customizable subtitle and action button

---
+++ components/TodayVerse.tsx
@@ -0,0 +1,98 @@
+import { BoldText } from "@/components/text/BoldText";
+import { MediumText } from "@/components/text/MediumText";
+import { RegularText } from "@/components/text/RegularText";
+import { moderateScale } from "@/utils/style";
+import React from 'react';
+import { StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
+import CustomButton from "./button/CustomButton";
+
+interface TodayVerseProps {
+  subTitle: string;
+  content: string;
+  style?: StyleProp<ViewStyle>;
+  onPress?: () => void;
+}
+
+export default function TodayVerse({ subTitle, content, style, onPress }: TodayVerseProps) {
+  return (
+    <View style={[styles.container, style]}>
+      {/* Title */}
+      <BoldText
+        style={styles.title}
+        color="#FFFFFF"
+        fontSize={16}
+        lineHeight={24}
+        letterSpacingPercent={-1}
+      >
+        오늘의 말씀
+      </BoldText>
+
+      {/* Card */}
+      <View style={styles.card}>
+        {/* subTitle */}
+        <MediumText
+          style={styles.subTitle}
+          color="#B3B3B3"
+          fontSize={12}
+          lineHeight={22}
+          letterSpacingPercent={-1}
+        >
+          {subTitle}
+        </MediumText>
+
+        {/* content */}
+        <RegularText
+          style={styles.content}
+          color="#FFFFFF"
+          fontSize={16}
+          lineHeight={24}
+          letterSpacingPercent={-1}
+        >
+          {content}
+        </RegularText>
+        {/* button */}
+        <CustomButton
+          style={styles.button}
+          onPress={onPress}
+        >
+          <MediumText
+            color="#FFFFFF"
+            fontSize={14}
+            lineHeight={21}
+            letterSpacingPercent={-1}
+          >
+            기도 시작하기
+          </MediumText>
+        </CustomButton>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: moderateScale(36),
+  },
+  title: {
+    marginBottom: moderateScale(12),
+  },
+  card: {
+    paddingVertical: moderateScale(18),
+    paddingHorizontal: moderateScale(16),
+    borderRadius: moderateScale(10),
+    backgroundColor: "rgba(255, 255, 255, 0.05)",
+  },
+  subTitle: {
+    marginBottom: moderateScale(4),
+  },
+  content: {
+    marginBottom: moderateScale(20),
+  },
+  button: {
+    width: 'auto',
+    alignSelf: 'flex-start',
+    backgroundColor: '#0F141A',
+    paddingVertical: moderateScale(12),
+    paddingHorizontal: moderateScale(24),
+  },
+});
