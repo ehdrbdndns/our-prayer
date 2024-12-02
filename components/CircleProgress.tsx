c9c3c8ce688020f447bff69c4373c08f02449727
ehdrbdndns <ehdrbdndns@naver.com>
2024-12-03 00:22:17 +0900

    feat: add CircleProgress component for displaying circular progress with gradient background

---
+++ components/CircleProgress.tsx
@@ -0,0 +1,70 @@
+import { moderateScale } from "@/utils/style";
+import { ImageBackground } from "expo-image";
+import { StyleSheet, View } from "react-native";
+import { ColorFormat } from "react-native-countdown-circle-timer";
+import Svg, { Defs, LinearGradient, Path, Stop } from "react-native-svg";
+
+const CircleProgressBackground = require('@/assets/images/circle-progress-background.png');
+
+type CircleProgressProps = {
+  elapsedTime: number
+  path: string
+  pathLength: number
+  remainingTime: number
+  rotation: 'clockwise' | 'counterclockwise'
+  size: number
+  stroke: ColorFormat
+  strokeDashoffset: number
+  strokeWidth: number,
+  duration: number
+}
+
+export default function CircleProgress(props: CircleProgressProps) {
+
+  const {
+    elapsedTime,
+    path,
+    pathLength,
+    rotation,
+    size,
+    stroke,
+    strokeDashoffset,
+    strokeWidth,
+    duration
+  } = props;
+
+  return (
+    <ImageBackground
+      source={CircleProgressBackground}
+      style={styles.container}
+    >
+      <View style={{ width: size, height: size, position: 'relative' }}>
+        <Svg width={size} height={size}>
+          <Defs>
+            <LinearGradient id="gradientColor" x1="1" y1="0" x2="0" y2="0">
+              <Stop offset="0%" stopColor="#7781A0" />
+              <Stop offset="100%" stopColor="#4F5FFF" />
+            </LinearGradient>
+          </Defs>
+          <Path
+            d={path}
+            stroke={stroke}
+            fill="none"
+            strokeWidth={strokeWidth}
+            strokeDasharray={pathLength}
+            strokeDashoffset={pathLength - strokeDashoffset}
+          />
+        </Svg>
+      </View>
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: moderateScale(212),
+    height: moderateScale(212),
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
