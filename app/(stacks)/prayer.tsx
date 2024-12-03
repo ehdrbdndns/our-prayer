f2b625713b2f099afec6fabc8e7d122c0c22da37
ehdrbdndns <ehdrbdndns@naver.com>
2024-12-03 22:30:31 +0900

    feat: refactor Prayer component to toggle visibility based on mode for text and timer sections

--- app/(stacks)/prayer.tsx
+++ app/(stacks)/prayer.tsx
@@ -171,52 +171,41 @@ export default function Prayer() {
           </View>
 
           {/* Text */}
-          {
-            mode === "text" && (
-              <>
-                <View style={[styles.textContainer]}>
-                  <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    contentContainerStyle={styles.scrollViewContainer}
-                  >
-                    <BoldText
-                      fontSize={24}
-                      lineHeight={40}
-                      color="rgba(255, 255, 255, 0.8)"
-                      textAlign="left"
-                    >
-                      {"사랑하는 하나님, 이 시간 저를 당신의 품으로 초대해 주셔서 감사합니다. 제 마음을 정돈하고, 당신과의 깊은 교제를 위해 준비합니다. 제가 가진 모든 걱정과 불안을 내려놓고, 오직 당신께 집중할 수 있도록 도와주세요.\n주님, 오늘 제가 기도하는 이 순간, 저의 마음 속 깊은 곳에 있는 소원과 갈망을 당신께 올립니다. 제가 사랑하는 이들을 위해 기도합니다. 그들에게 필요한 힘과 위로를 주시고, 당신의 사랑이 그들의 삶을 가득 채우게 하여 주세요. 하나님, 저의 삶의 방향과 선택을 인도해 주시기를 간구합니다. 제가 걸어가는 길에서 당신의 뜻을 발견하고, 그 길을 따르게 하여 주세요. 매일의 삶 속에서 당신의 은혜를 느낄 수 있도록 해주시고, 모든 순간에 감사하는 마음을 갖게 하여 주세요. 주님, 지금 이 순간, 세상의 고통과 아픔을 위해 기도합니다. 힘든 상황에 처한 이들에게 위로와 소망을 주시고, 당신의 사랑이 그들에게 전해지기를 간구합니다. 우리가 서로를 사랑하고 도우며 살아갈 수 있도록 인도해 주세요. 하나님, 당신의 말씀과 진리로 저를 가르쳐 주세요. 기도를 통해 제 영혼이 새롭게 되고, 당신과의 관계가 더욱 깊어지기를 소망합니다. 기도의 시간이 저에게 힘과 평화를 주는 시간이 되게 하여 주세요. 이제 제가 드리는 모든 기도를 당신의 뜻에 맡깁니다. 제가 기도하는 이 순간, 당신의 사랑이 저를 감싸고, 당신의 은혜가 제 삶을 인도하기를 바랍니다. 감사합니다, 주님. 아멘."}
-                    </BoldText>
-                  </ScrollView>
-                </View>
-                <LinearGradient
-                  start={{ x: 0.5, y: 0 }}
-                  colors={['transparent', 'rgba(43, 47, 58, 1)']}
-                  style={styles.textFilter}
-                />
-              </>
-            )
-          }
+          <View style={[styles.textContainer, mode === 'default' && styles.hidden]}>
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={styles.scrollViewContainer}
+            >
+              <BoldText
+                fontSize={24}
+                lineHeight={40}
+                color="rgba(255, 255, 255, 0.8)"
+                textAlign="left"
+              >
+                {"사랑하는 하나님, 이 시간 저를 당신의 품으로 초대해 주셔서 감사합니다. 제 마음을 정돈하고, 당신과의 깊은 교제를 위해 준비합니다. 제가 가진 모든 걱정과 불안을 내려놓고, 오직 당신께 집중할 수 있도록 도와주세요.\n주님, 오늘 제가 기도하는 이 순간, 저의 마음 속 깊은 곳에 있는 소원과 갈망을 당신께 올립니다. 제가 사랑하는 이들을 위해 기도합니다. 그들에게 필요한 힘과 위로를 주시고, 당신의 사랑이 그들의 삶을 가득 채우게 하여 주세요. 하나님, 저의 삶의 방향과 선택을 인도해 주시기를 간구합니다. 제가 걸어가는 길에서 당신의 뜻을 발견하고, 그 길을 따르게 하여 주세요. 매일의 삶 속에서 당신의 은혜를 느낄 수 있도록 해주시고, 모든 순간에 감사하는 마음을 갖게 하여 주세요. 주님, 지금 이 순간, 세상의 고통과 아픔을 위해 기도합니다. 힘든 상황에 처한 이들에게 위로와 소망을 주시고, 당신의 사랑이 그들에게 전해지기를 간구합니다. 우리가 서로를 사랑하고 도우며 살아갈 수 있도록 인도해 주세요. 하나님, 당신의 말씀과 진리로 저를 가르쳐 주세요. 기도를 통해 제 영혼이 새롭게 되고, 당신과의 관계가 더욱 깊어지기를 소망합니다. 기도의 시간이 저에게 힘과 평화를 주는 시간이 되게 하여 주세요. 이제 제가 드리는 모든 기도를 당신의 뜻에 맡깁니다. 제가 기도하는 이 순간, 당신의 사랑이 저를 감싸고, 당신의 은혜가 제 삶을 인도하기를 바랍니다. 감사합니다, 주님. 아멘."}
+              </BoldText>
+            </ScrollView>
+          </View>
+          <LinearGradient
+            style={[styles.textFilter, mode === 'default' && styles.hidden]}
+            start={{ x: 0.5, y: 0 }}
+            colors={['transparent', 'rgba(43, 47, 58, 1)']}
+          />
 
           {/* Timer */}
-          {
-            mode === "default" && (
-              <View style={styles.timer}>
-                <Timer
-                  key={timerKey}
-                  repeatCount={repeatCount}
-                  duration={DEFAULT_DURATION}
-                  initialRemainingTime={initialRemainingTime}
-                  isPlaying={isPlaying}
-                  onPressNext={onPressNext}
-                  onPressPlay={onPressPlay}
-                  onPressPrev={onPressPrev}
-                  onComplete={onCompleteTimer}
-                />
-              </View>
-            )
-          }
-
+          <View style={[styles.timer, mode === 'text' && styles.hidden]}>
+            <Timer
+              key={timerKey}
+              repeatCount={repeatCount}
+              duration={DEFAULT_DURATION}
+              initialRemainingTime={initialRemainingTime}
+              isPlaying={isPlaying}
+              onPressNext={onPressNext}
+              onPressPlay={onPressPlay}
+              onPressPrev={onPressPrev}
+              onComplete={onCompleteTimer}
+            />
+          </View>
         </Animated.View>
       )
       }
