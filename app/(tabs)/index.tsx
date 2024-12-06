c8626079dc3bb0050f88d5dd258373618d140416
ehdrbdndns <ehdrbdndns@naver.com>
2024-12-06 12:41:17 +0900

    feat: add CustomButton for viewing all prayer records in the Index component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import Fire from "@/assets/images/icon/fire.svg";
 import Search from "@/assets/images/icon/search.svg";
 import Star from "@/assets/images/icon/star.svg";
 import Logo from "@/assets/images/text-s-logo.svg";
+import CustomButton from "@/components/button/CustomButton";
 import Header from "@/components/Header";
 import PrayerPlan from "@/components/PrayerPlan";
 import PrayerRecord from "@/components/PrayerRecord";
@@ -81,8 +82,38 @@ export default function Index() {
         </View>
 
         {/* 기도 일자 데이터 */}
-        <View style={styles.content}>
+        <View style={[styles.content, { marginBottom: moderateScale(40) }]}>
+          {/* Title */}
+          <BoldText
+            style={{ marginBottom: moderateScale(16) }}
+            color="#FFFFFF"
+            fontSize={16}
+            lineHeight={24}
+            letterSpacingPercent={-1}
+          >
+            나의 기도 기록
+          </BoldText>
+
           <PrayerRecord />
+
+          {/* Button */}
+          <CustomButton style={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            backgroundColor: "rgba(255, 255, 255, 0.05)",
+            paddingVertical: moderateScale(12),
+            paddingHorizontal: moderateScale(24),
+            marginTop: moderateScale(16),
+          }}>
+            <BoldText
+              color="#FFFFFF"
+              fontSize={14}
+              lineHeight={22}
+              letterSpacingPercent={-1}
+            >
+              기도 기록 전체보기
+            </BoldText>
+          </CustomButton>
         </View>
 
         {/* 기도 플랜 */}
