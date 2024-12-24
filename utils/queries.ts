2ae2da82ccd329824c991589f3417d97274b9a57
ehdrbdndns <ehdrbdndns@naver.com>
2024-12-24 16:48:39 +0900

    feat: add useLectureQuery hook to fetch lecture data with dynamic lecture_id

--- utils/queries.ts
+++ utils/queries.ts
@@ -1,5 +1,5 @@
 import api from '@/utils/axios';
-import { PlanDetailResponseType, PlanResponseType } from '@/utils/dataType';
+import { LectureResponseType, PlanDetailResponseType, PlanResponseType } from '@/utils/dataType';
 import { useQuery } from '@tanstack/react-query';
 import * as SecureStore from 'expo-secure-store';
 
@@ -59,6 +59,35 @@ export const usePlanQuery = ({ plan_id }: { plan_id: string }) => {
           is_liked: Boolean(Number(res.data.plan.is_liked))
         }
       };
+    },
+    staleTime: 12 * 60 * 60 * 1000, // 12시간
+    gcTime: 12 * 60 * 60 * 1000, // 12시간
+  });
+};
+
+export const useLectureQuery = ({ lecture_id }: { lecture_id: string }) => {
+  return useQuery<LectureResponseType>({
+    queryKey: ["lecture", lecture_id],
+    queryFn: async () => {
+      const accessToken = await SecureStore.getItemAsync("accessToken");
+      const refreshToken = await SecureStore.getItemAsync("refreshToken");
+
+      const res = await api.get<LectureResponseType>(`/lecture?lecture_id=${lecture_id}`, {
+        headers: {
+          "Authorization": `Bearer ${accessToken}`,
+          "RefreshToken": refreshToken
+        }
+      });
+
+      const data = {
+        ...res.data,
+        lecture: {
+          ...res.data.lecture,
+          time: Number(res.data.lecture.time)
+        }
+      }
+
+      return data;
     },
     staleTime: 12 * 60 * 60 * 1000, // 12시간
     gcTime: 12 * 60 * 60 * 1000, // 12시간
