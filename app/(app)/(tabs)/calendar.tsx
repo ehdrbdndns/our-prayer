e13d314aeb7ad9a4f80b246082069c8f391f6523
ehdrbdndns <ehdrbdndns@naver.com>
2024-12-30 19:30:01 +0900

    feat: disable vertical scroll indicator in CalendarPage

--- app/(app)/(tabs)/calendar.tsx
+++ app/(app)/(tabs)/calendar.tsx
@@ -161,7 +161,9 @@ export default function CalendarPage() {
   }
 
   return (
-    <ScrollView>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+    >
       <SafeAreaView>
         {/* Header */}
         <Header
