1f42d0fb0ea9587d72feaa06c06a4dffb6a449b1
ehdrbdndns <ehdrbdndns@naver.com>
2025-01-14 00:17:53 +0900

    feat: add push notification registration utility in notification.ts

---
+++ utils/notification.ts
@@ -0,0 +1,48 @@
+import Constants from 'expo-constants';
+import * as Device from "expo-device";
+import { AndroidImportance, getExpoPushTokenAsync, getPermissionsAsync, requestPermissionsAsync, setNotificationChannelAsync } from "expo-notifications";
+import { Platform } from "react-native";
+
+export async function registerForPushNotificationsAsync() {
+  if (Platform.OS === 'android') {
+    setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      return '';
+    }
+
+    // need to test this
+    const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+    // const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+
+    if (!projectId) {
+      throw new Error('No project ID found');
+    }
+    try {
+      const pushTokenString = (
+        await getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+
+      return pushTokenString;
+    } catch (e: unknown) {
+      throw new Error(e as string);
+    }
+  } else {
+    throw new Error('Must use physical device for push notifications');
+  }
+}
