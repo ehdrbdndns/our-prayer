95d22c56a784a6a5b79bb909cda96b47c695d726
ehdrbdndns <ehdrbdndns@naver.com>
2025-01-30 20:20:38 +0900

    fix: disable pointer events on gradient overlay in Lecture component

--- app/(app)/(stacks)/lectureDetail/[lecture_id].tsx
+++ app/(app)/(stacks)/lectureDetail/[lecture_id].tsx
@@ -325,6 +325,7 @@ export default function Lecture() {
             style={[styles.textFilter, mode === 'default' && styles.hidden]}
             start={{ x: 0.5, y: 0 }}
             colors={['transparent', 'rgba(43, 47, 58, 1)']}
+            pointerEvents="none"
           />
 
           {/* Timer */}
