b8f1b854bab2df9f6ef08687a92d6add48813175
ehdrbdndns <ehdrbdndns@naver.com>
2025-03-24 20:39:02 +0900

    feat: enhance amp functionality with error handling for audio playback

--- app/(app)/(stacks)/lectureDetail/[lecture_id].tsx
+++ app/(app)/(stacks)/lectureDetail/[lecture_id].tsx
@@ -176,7 +176,15 @@ export default function Lecture() {
 
     async function turnOnAmp() {
       if (!amp) return;
-      await amp.turnOn();
+
+      const isAmpTurnedOn = await amp.turnOn();
+
+      if (!isAmpTurnedOn) {
+        Alert.alert('알림!', '오디오 파일을 다시 다운로드 해주세요.');
+        await AsyncStorage.removeItem(`planAudit-${plan_id}`);
+        router.dismissTo('/plan');
+      }
+
       await amp.playBgm();
     }
 
