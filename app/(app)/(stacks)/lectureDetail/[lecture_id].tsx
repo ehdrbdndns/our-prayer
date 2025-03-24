fad7667f5382dc54e81ca99b71adc642b6f71ebc
ehdrbdndns <ehdrbdndns@naver.com>
2025-03-24 20:49:06 +0900

    feat: improve audio alert handling with confirmation button for re-download

--- app/(app)/(stacks)/lectureDetail/[lecture_id].tsx
+++ app/(app)/(stacks)/lectureDetail/[lecture_id].tsx
@@ -180,9 +180,15 @@ export default function Lecture() {
       const isAmpTurnedOn = await amp.turnOn();
 
       if (!isAmpTurnedOn) {
-        Alert.alert('알림!', '오디오 파일을 다시 다운로드 해주세요.');
-        await AsyncStorage.removeItem(`planAudit-${plan_id}`);
-        router.dismissTo('/plan');
+        Alert.alert('알림!', '오디오 파일을 다시 다운로드 해주세요.', [
+          {
+            text: '확인',
+            onPress: async () => {
+              await AsyncStorage.removeItem(`planAudit-${plan_id}`);
+              router.dismissTo('/plan');
+            }
+          }
+        ]);
       }
 
       await amp.playBgm();
